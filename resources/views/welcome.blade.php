39ab0427b54d6e05170c36265b8e051adc2b1c9d
Emma Sarkilla <sarkillaemma@gmail.com>
2025-07-31 14:52:17 +0700

    bismillah aamiin 10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -184,7 +184,7 @@
 
         .header {
             background: linear-gradient(to right, #0f2027cc, #203a43cc, #2c5364cc), 
-                        url('{{ secure_asset('iamges/imigrasi.jpg') }}') no-repeat center center;
+                        url('{{ asset('images/imigrasi.jpg') }}') no-repeat center center;
             background-size: cover;
             color: white;
             position: relative;
